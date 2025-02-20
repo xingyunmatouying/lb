1167e921e9ffa6fe5558bca4cf8789b1abb7c991
Eirik Ogilvie-Wigley <eowigley@gmail.com>
2025-02-19 18:11:43 -0700

    Fix docstring warnings

--- src/generate/lichess_client.py
+++ src/generate/lichess_client.py
@@ -1,12 +1,10 @@
-"""
-Client for communicating with lichess.
-"""
+"""Client for communicating with lichess."""
 
 import abc
 
 
 class LichessClient(abc.ABC):
-  """Interface for communicating with lichess"""
+  """Interface for communicating with lichess."""
 
   @abc.abstractmethod
   def get_online_bots(self) -> str:
