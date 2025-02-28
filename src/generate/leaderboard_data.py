223a53d1f467baf31d448a33e3fd61e08f411255
Eirik Ogilvie-Wigley <eowigley@gmail.com>
2025-02-27 21:15:43 -0700

    Add leaderboard updated classes

--- src/generate/leaderboard_data.py
+++ src/generate/leaderboard_data.py
@@ -1,5 +1,6 @@
 """Leaderboard-related dataclasses."""
 
+import abc
 import dataclasses
 
 from src.generate.lichess_bot_user import BotUser, Perf
@@ -134,3 +135,120 @@ class LeaderboardRow:
       previous_rating = perf.rating
 
     return leaderboard_rows
+
+
+class LeaderboardUpdate(abc.ABC):
+  """The information required to update a row in the leaderboard."""
+
+  @abc.abstractmethod
+  def get_rating(self) -> int:
+    """Return the bot's rating."""
+    ...
+
+  @abc.abstractmethod
+  def get_created_date(self) -> str:
+    """Return the date the bot was created."""
+    ...
+
+  @abc.abstractmethod
+  def to_leaderboard_row(self, rank: int) -> LeaderboardRow:
+    """Convert the update information into a leaderboard row."""
+    ...
+
+  @classmethod
+  def from_previous_row_and_current_perf(
+    cls, previous_row: LeaderboardRow | None, current_perf: LeaderboardPerf | None
+  ) -> "LeaderboardUpdate":
+    """Return the specific type of update based on the presence of previous_row and current_perf.
+
+    If both parameters are None, a ValueError will be raised.
+    """
+    if previous_row and not current_perf:
+      return PreviousRowOnlyUpdate(previous_row)
+    if current_perf and not previous_row:
+      return CurrentPerfOnlyUpdate(current_perf)
+    if previous_row and current_perf:
+      return FullUpdate(previous_row, current_perf)
+    raise ValueError("At least one of previous_row or current_perf must be set.")
+
+
+@dataclasses.dataclass(frozen=True)
+class PreviousRowOnlyUpdate(LeaderboardUpdate):
+  """Only the previous row was found.
+
+  This happens when the bot has been seen previously but was not found now.
+  """
+
+  row: LeaderboardRow
+
+  def get_rating(self) -> int:
+    """Return the bot's rating."""
+    return self.row.perf.rating
+
+  def get_created_date(self) -> str:
+    """Return the date the bot was created."""
+    return self.row.perf.created_date
+
+  def to_leaderboard_row(self, rank: int) -> LeaderboardRow:
+    """Convert the update information into a leaderboard row."""
+    rank_delta = self.row.rank - rank
+    rating_delta = 0
+    peak_rank = min(self.row.rank, rank)
+    peak_rating = self.row.peak_rating
+    return LeaderboardRow(self.row.perf, rank, rank_delta, rating_delta, peak_rank, peak_rating)
+
+
+@dataclasses.dataclass(frozen=True)
+class CurrentPerfOnlyUpdate(LeaderboardUpdate):
+  """Only the previous row was found.
+
+  This happens when the bot has been seen previously but was not found now.
+  """
+
+  perf: LeaderboardPerf
+
+  def get_rating(self) -> int:
+    """Return the bot's rating."""
+    return self.perf.rating
+
+  def get_created_date(self) -> str:
+    """Return the date the bot was created."""
+    return self.perf.created_date
+
+  def to_leaderboard_row(self, rank: int) -> LeaderboardRow:
+    """Convert the update information into a leaderboard row."""
+    # TODO it would be nice if instead of 0, there were some indication that this is a new bot.
+    rank_delta = 0
+    rating_delta = 0
+    peak_rank = rank
+    peak_rating = self.perf.rating
+    return LeaderboardRow(self.perf, rank, rank_delta, rating_delta, peak_rank, peak_rating)
+
+
+@dataclasses.dataclass(frozen=True)
+class FullUpdate(LeaderboardUpdate):
+  """The bot is already on the leaderboard and we have new data."""
+
+  previous_row: LeaderboardRow
+
+  current_perf: LeaderboardPerf
+
+  def get_rating(self) -> int:
+    """Return the bot's rating."""
+    # Use the current rating
+    return self.current_perf.rating
+
+  def get_created_date(self) -> str:
+    """Return the date the bot was created."""
+    # The old and new created dates are expected to be the same
+    return self.current_perf.created_date
+
+  def to_leaderboard_row(self, rank: int) -> LeaderboardRow:
+    """Convert the update information into a leaderboard row."""
+    # Moving up in the leaderboard should count as a positive delta (3 -> 1 yields +2)
+    rank_delta = self.previous_row.rank - rank
+    rating_delta = self.current_perf.rating - self.previous_row.perf.rating
+    # Higher ranking, lower rank number
+    peak_rank = min(self.previous_row.rank, rank)
+    peak_rating = max(self.previous_row.perf.rating, self.current_perf.rating)
+    return LeaderboardRow(self.current_perf, rank, rank_delta, rating_delta, peak_rank, peak_rating)
--- tests/generate/test_leaderboard_data.py
+++ tests/generate/test_leaderboard_data.py
@@ -2,24 +2,34 @@
 
 import unittest
 
-from src.generate.leaderboard_data import LeaderboardPerf, LeaderboardRow
+from src.generate.leaderboard_data import (
+  CurrentPerfOnlyUpdate,
+  FullUpdate,
+  LeaderboardPerf,
+  LeaderboardRow,
+  LeaderboardUpdate,
+  PreviousRowOnlyUpdate,
+)
 from src.generate.lichess_bot_user import BotUser, Perf, PerfType
 
 
-TEST_BULLET_PERF = Perf(PerfType.BULLET, 100, 1450, False)
-TEST_BOT_USER = BotUser("Bot1", [TEST_BULLET_PERF])
+TEST_BULLET_LICHESS_PERF = Perf(PerfType.BULLET, 100, 1450, False)
+TEST_BOT_USER = BotUser("Bot1", [TEST_BULLET_LICHESS_PERF])
 
-TOP_BOT = LeaderboardPerf("Top Bot", 1800, 400, "2021-04-01", "2025-10-12")
-FIRST_MIDDLE_BOT = LeaderboardPerf("First Middle Bot", 1500, 100, "2022-04-01", "2025-10-12")
-SECOND_MIDDLE_BOT = LeaderboardPerf("Second Middle Bot", 1500, 200, "2023-04-01", "2025-10-12")
-BOTTOM_BOT = LeaderboardPerf("Bottom Bot", 1200, 300, "2025-04-01", "2025-10-12")
+TOP_BOT_PERF = LeaderboardPerf("Top Bot", 1800, 400, "2021-04-01", "2025-10-12")
+FIRST_MIDDLE_BOT_PERF = LeaderboardPerf("First Middle Bot", 1500, 100, "2022-04-01", "2025-10-12")
+SECOND_MIDDLE_BOT_PERF = LeaderboardPerf("Second Middle Bot", 1500, 200, "2023-04-01", "2025-10-12")
+BOTTOM_BOT_PERF = LeaderboardPerf("Bottom Bot", 1200, 300, "2025-04-01", "2025-10-12")
+
+
+TOP_BOT_ROW = LeaderboardRow(TOP_BOT_PERF, 1, 0, 0, 1, 1500)
 
 
 class TestLeaderboardPerf(unittest.TestCase):
   """Tests for LeaderboardPeft."""
 
   def test_from_bot_user(self) -> None:
-    leaderboard_perf = LeaderboardPerf.from_bot_user(TEST_BOT_USER, TEST_BULLET_PERF)
+    leaderboard_perf = LeaderboardPerf.from_bot_user(TEST_BOT_USER, TEST_BULLET_LICHESS_PERF)
     self.assertEqual(leaderboard_perf, LeaderboardPerf("Bot1", 1450, 100, "TODO", "TODO"))
 
 
@@ -39,29 +49,65 @@ class TestLeaderboardRow(unittest.TestCase):
     self.assertEqual(leaderboard_row.to_psv(), expected_psv)
 
   def test_create_leaderboard_rows_with_one_perf(self) -> None:
-    leaderboard_rows = LeaderboardRow.create_leaderboard_rows([TOP_BOT])
+    leaderboard_rows = LeaderboardRow.create_leaderboard_rows([TOP_BOT_PERF])
     self.assertEqual(len(leaderboard_rows), 1)
-    self.assertEqual(leaderboard_rows[0], LeaderboardRow(TOP_BOT, 1, 0, 0, 1, 1800))
+    self.assertEqual(leaderboard_rows[0], LeaderboardRow(TOP_BOT_PERF, 1, 0, 0, 1, 1800))
 
   def test_create_leaderboard_rows_with_three_perfs(self) -> None:
-    leaderboard_rows = LeaderboardRow.create_leaderboard_rows([FIRST_MIDDLE_BOT, BOTTOM_BOT, TOP_BOT])
+    leaderboard_rows = LeaderboardRow.create_leaderboard_rows([FIRST_MIDDLE_BOT_PERF, BOTTOM_BOT_PERF, TOP_BOT_PERF])
     self.assertEqual(len(leaderboard_rows), 3)
-    self.assertEqual(leaderboard_rows[0], LeaderboardRow(TOP_BOT, 1, 0, 0, 1, 1800))
-    self.assertEqual(leaderboard_rows[1], LeaderboardRow(FIRST_MIDDLE_BOT, 2, 0, 0, 2, 1500))
-    self.assertEqual(leaderboard_rows[2], LeaderboardRow(BOTTOM_BOT, 3, 0, 0, 3, 1200))
+    self.assertEqual(leaderboard_rows[0], LeaderboardRow(TOP_BOT_PERF, 1, 0, 0, 1, 1800))
+    self.assertEqual(leaderboard_rows[1], LeaderboardRow(FIRST_MIDDLE_BOT_PERF, 2, 0, 0, 2, 1500))
+    self.assertEqual(leaderboard_rows[2], LeaderboardRow(BOTTOM_BOT_PERF, 3, 0, 0, 3, 1200))
 
   def test_create_leaderboard_rows_with_tied_ratings(self) -> None:
-    leaderboard_rows = LeaderboardRow.create_leaderboard_rows([SECOND_MIDDLE_BOT, BOTTOM_BOT, TOP_BOT, FIRST_MIDDLE_BOT])
+    leaderboard_rows = LeaderboardRow.create_leaderboard_rows(
+      [SECOND_MIDDLE_BOT_PERF, BOTTOM_BOT_PERF, TOP_BOT_PERF, FIRST_MIDDLE_BOT_PERF]
+    )
     self.assertEqual(len(leaderboard_rows), 4)
-    self.assertEqual(leaderboard_rows[0], LeaderboardRow(TOP_BOT, 1, 0, 0, 1, 1800))
-    self.assertEqual(leaderboard_rows[1], LeaderboardRow(FIRST_MIDDLE_BOT, 2, 0, 0, 2, 1500))
-    self.assertEqual(leaderboard_rows[2], LeaderboardRow(SECOND_MIDDLE_BOT, 2, 0, 0, 2, 1500))
-    self.assertEqual(leaderboard_rows[3], LeaderboardRow(BOTTOM_BOT, 4, 0, 0, 4, 1200))
+    self.assertEqual(leaderboard_rows[0], LeaderboardRow(TOP_BOT_PERF, 1, 0, 0, 1, 1800))
+    self.assertEqual(leaderboard_rows[1], LeaderboardRow(FIRST_MIDDLE_BOT_PERF, 2, 0, 0, 2, 1500))
+    self.assertEqual(leaderboard_rows[2], LeaderboardRow(SECOND_MIDDLE_BOT_PERF, 2, 0, 0, 2, 1500))
+    self.assertEqual(leaderboard_rows[3], LeaderboardRow(BOTTOM_BOT_PERF, 4, 0, 0, 4, 1200))
 
   def test_create_leaderboard_rows_with_three_way_tie(self) -> None:
-    leaderboard_rows = LeaderboardRow.create_leaderboard_rows([BOTTOM_BOT, TOP_BOT, TOP_BOT, TOP_BOT])
+    leaderboard_rows = LeaderboardRow.create_leaderboard_rows([BOTTOM_BOT_PERF, TOP_BOT_PERF, TOP_BOT_PERF, TOP_BOT_PERF])
     self.assertEqual(len(leaderboard_rows), 4)
-    self.assertEqual(leaderboard_rows[0], LeaderboardRow(TOP_BOT, 1, 0, 0, 1, 1800))
-    self.assertEqual(leaderboard_rows[1], LeaderboardRow(TOP_BOT, 1, 0, 0, 1, 1800))
-    self.assertEqual(leaderboard_rows[1], LeaderboardRow(TOP_BOT, 1, 0, 0, 1, 1800))
-    self.assertEqual(leaderboard_rows[3], LeaderboardRow(BOTTOM_BOT, 4, 0, 0, 4, 1200))
+    self.assertEqual(leaderboard_rows[0], LeaderboardRow(TOP_BOT_PERF, 1, 0, 0, 1, 1800))
+    self.assertEqual(leaderboard_rows[1], LeaderboardRow(TOP_BOT_PERF, 1, 0, 0, 1, 1800))
+    self.assertEqual(leaderboard_rows[1], LeaderboardRow(TOP_BOT_PERF, 1, 0, 0, 1, 1800))
+    self.assertEqual(leaderboard_rows[3], LeaderboardRow(BOTTOM_BOT_PERF, 4, 0, 0, 4, 1200))
+
+
+class TestLeaderboardUpdate(unittest.TestCase):
+  """Tests for LeaderboardUpdate and subclasses."""
+
+  def test_from_previous_row_and_current_perf(self) -> None:
+    self.assertIsInstance(LeaderboardUpdate.from_previous_row_and_current_perf(TOP_BOT_ROW, TOP_BOT_PERF), FullUpdate)
+    self.assertIsInstance(LeaderboardUpdate.from_previous_row_and_current_perf(TOP_BOT_ROW, None), PreviousRowOnlyUpdate)
+    self.assertIsInstance(LeaderboardUpdate.from_previous_row_and_current_perf(None, TOP_BOT_PERF), CurrentPerfOnlyUpdate)
+    self.assertRaises(ValueError, lambda: LeaderboardUpdate.from_previous_row_and_current_perf(None, None))
+
+  def test_row_only_update(self) -> None:
+    previous_perf = LeaderboardPerf("Bot 1", 1500, 100, "2024-01-01", "2025-01-01")
+    previous_row = LeaderboardRow(previous_perf, 5, 0, 0, 3, 1500)
+    update = PreviousRowOnlyUpdate(previous_row)
+    self.assertEqual(update.get_rating(), 1500)
+    self.assertEqual(update.get_created_date(), "2024-01-01")
+    self.assertEqual(update.to_leaderboard_row(2), LeaderboardRow(previous_perf, 2, 3, 0, 2, 1500))
+
+  def test_perf_only_update(self) -> None:
+    previous_perf = LeaderboardPerf("Bot 1", 1500, 100, "2024-01-01", "2025-01-01")
+    update = CurrentPerfOnlyUpdate(previous_perf)
+    self.assertEqual(update.get_rating(), 1500)
+    self.assertEqual(update.get_created_date(), "2024-01-01")
+    self.assertEqual(update.to_leaderboard_row(2), LeaderboardRow(previous_perf, 2, 0, 0, 2, 1500))
+
+  def test_full_update(self) -> None:
+    previous_perf = LeaderboardPerf("Bot 1", 1500, 100, "2024-01-01", "2025-01-01")
+    previous_row = LeaderboardRow(previous_perf, 5, 0, 0, 3, 1500)
+    current_perf = LeaderboardPerf("Bot 1", 1600, 120, "2024-01-01", "2024-01-02")
+    update = FullUpdate(previous_row, current_perf)
+    self.assertEqual(update.get_rating(), 1600)
+    self.assertEqual(update.get_created_date(), "2024-01-01")
+    self.assertEqual(update.to_leaderboard_row(1), LeaderboardRow(current_perf, 1, 4, 100, 1, 1600))
