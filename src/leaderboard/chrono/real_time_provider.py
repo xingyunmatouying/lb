3898acf02b5c2f60f546c8afdc707c924fea9fdf
Eirik Ogilvie-Wigley <eowigley@gmail.com>
2025-03-18 00:32:20 -0600

    Fix RealTimeProvider

--- src/leaderboard/chrono/real_time_provider.py
+++ src/leaderboard/chrono/real_time_provider.py
@@ -1,11 +1,11 @@
-"""An implementation of DateProvider which provides the actual current time."""
+"""An implementation of TimeProvider which provides the actual current time."""
 
 import time
 
 from src.leaderboard.chrono.time_provider import TimeProvider
 
 
-class RealDateProvider(TimeProvider):
+class RealTimeProvider(TimeProvider):
   """Provides the current time."""
 
   def get_current_time(self) -> float:
