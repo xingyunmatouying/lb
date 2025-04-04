6d8e9bb0c84f5a0bbb9b884910297ebaedbf6ae8
Eirik Ogilvie-Wigley <eowigley@gmail.com>
2025-04-04 14:45:17 -0600

    Add special handling for when it's the bots creation day and month

--- tests/leaderboard/chrono/test_duration_formatter.py
+++ tests/leaderboard/chrono/test_duration_formatter.py
@@ -6,9 +6,9 @@ from src.leaderboard.chrono import duration_formatter
 from tests.leaderboard.chrono import epoch_seconds
 
 
-DATE_2024_05_01 = epoch_seconds.from_date(2024, 5, 1)
-DATE_2024_04_02 = epoch_seconds.from_date(2024, 4, 2)
 DATE_2024_03_02 = epoch_seconds.from_date(2024, 3, 2)
+DATE_2024_04_02 = epoch_seconds.from_date(2024, 4, 2)
+DATE_2024_05_01 = epoch_seconds.from_date(2024, 5, 1)
 
 DATE_2025_03_02 = epoch_seconds.from_date(2025, 3, 2)
 DATE_2025_04_01 = epoch_seconds.from_date(2025, 4, 1)
@@ -23,3 +23,6 @@ class TestDurationFormatter(unittest.TestCase):
     self.assertEqual(duration_formatter.format_age(DATE_2024_03_02, DATE_2025_04_01), "1y 0mo")
     self.assertEqual(duration_formatter.format_age(DATE_2025_03_02, DATE_2025_04_01), "<1mo")
     self.assertEqual(duration_formatter.format_age(DATE_2025_04_01, DATE_2025_03_02), "<1mo")
+
+  def test_format_age_birthday(self) -> None:
+    self.assertEqual(duration_formatter.format_age(DATE_2024_03_02, DATE_2025_03_02), "🎂 1y 🎂")
