733cbe78717962fac177acd8974fef485a100bbc
Eirik Ogilvie-Wigley <eowigley@gmail.com>
2025-04-05 13:21:24 -0600

    Delete accidentally added method

--- src/leaderboard/fs/file_paths.py
+++ src/leaderboard/fs/file_paths.py
@@ -3,11 +3,6 @@
 from src.leaderboard.li.pert_type import PerfType
 
 
-def bot_profiles_data_path() -> str:
-  """Return "leaderboard_data/bot_profiles.ndjson"."""
-  return "leaderboard_data/bot_profiles.ndjson"
-
-
 def data_path(perf_type: PerfType) -> str:
   """Return "leaderboard_data/{perf_type.to_string()}.ndjson"."""
   return f"leaderboard_data/{perf_type.to_string()}.ndjson"
