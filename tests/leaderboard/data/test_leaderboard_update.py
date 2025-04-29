15d42adcb7a3347f85ebfe440dd360416e858dd7
Eirik Ogilvie-Wigley <eowigley@gmail.com>
2025-04-29 00:07:07 -0600

    Add tests for update get rd method

--- tests/leaderboard/data/test_leaderboard_update.py
+++ tests/leaderboard/data/test_leaderboard_update.py
@@ -12,9 +12,9 @@ from src.leaderboard.data.leaderboard_update import (
 from tests.leaderboard.chrono.epoch_seconds import DATE_2024_04_01, DATE_2025_04_01
 
 
-def create_bot_perf(name: str, rating: int, games: int) -> BotPerf:
+def create_bot_perf(name: str, rating: int, games: int, rd: int) -> BotPerf:
   """Create a BotPerf with several default values set."""
-  return BotPerf(name, LeaderboardPerf(rating, 0, 0, games, False))
+  return BotPerf(name, LeaderboardPerf(rating, rd, 0, games, False))
 
 
 class TestLeaderboardUpdate(unittest.TestCase):
@@ -25,7 +25,7 @@ class TestLeaderboardUpdate(unittest.TestCase):
     self.assertFalse(LeaderboardUpdate.check_is_eligible(True))
 
   def test_from_previous_row_and_current_bot_perf(self) -> None:
-    bot_perf = create_bot_perf("Bot 1", 1800, 400)
+    bot_perf = create_bot_perf("Bot 1", 1800, 400, 45)
     bot_row = LeaderboardRow("Bot 1", bot_perf.perf, RankInfo(1, 0, 0, 0, 1, 1800, DATE_2025_04_01))
     self.assertIsInstance(LeaderboardUpdate.create_update(bot_row, bot_perf, DATE_2025_04_01), FullUpdate)
     self.assertIsInstance(LeaderboardUpdate.create_update(bot_row, None, DATE_2025_04_01), PreviousRowOnlyUpdate)
@@ -33,37 +33,40 @@ class TestLeaderboardUpdate(unittest.TestCase):
     self.assertRaises(ValueError, lambda: LeaderboardUpdate.create_update(None, None, DATE_2025_04_01))
 
   def test_row_only_update(self) -> None:
-    previous_bot_perf = create_bot_perf("Bot 1", 1500, 100)
+    previous_bot_perf = create_bot_perf("Bot 1", 1500, 100, 45)
     previous_row = LeaderboardRow("Bot 1", previous_bot_perf.perf, RankInfo(5, -1, -10, 4, 3, 1500, DATE_2025_04_01))
     update = PreviousRowOnlyUpdate(previous_row)
-    self.assertEqual(update.get_rating(), 1500)
     self.assertEqual(update.get_name(), "Bot 1")
+    self.assertEqual(update.get_rating(), 1500)
+    self.assertEqual(update.get_rd(), 45)
     self.assertTrue(update.is_eligible())
     expected_row = LeaderboardRow("Bot 1", previous_bot_perf.perf, RankInfo(2, 3, 0, 0, 2, 1500, DATE_2025_04_01))
     self.assertEqual(update.to_leaderboard_row(2), expected_row)
 
   def test_bot_perf_only_update(self) -> None:
-    previous_bot_perf = create_bot_perf("Bot 1", 1500, 100)
+    previous_bot_perf = create_bot_perf("Bot 1", 1500, 100, 45)
     update = CurrentBotPerfOnlyUpdate(previous_bot_perf, DATE_2025_04_01)
-    self.assertEqual(update.get_rating(), 1500)
     self.assertEqual(update.get_name(), "Bot 1")
+    self.assertEqual(update.get_rating(), 1500)
+    self.assertEqual(update.get_rd(), 45)
     self.assertTrue(update.is_eligible())
     expected_row = LeaderboardRow("Bot 1", previous_bot_perf.perf, RankInfo(2, 0, 0, 0, 2, 1500, DATE_2025_04_01))
     self.assertEqual(update.to_leaderboard_row(2), expected_row)
 
   def test_full_update(self) -> None:
-    previous_bot_perf = create_bot_perf("Bot 1", 1500, 100)
+    previous_bot_perf = create_bot_perf("Bot 1", 1500, 100, 45)
     previous_row = LeaderboardRow("Bot 1", previous_bot_perf.perf, RankInfo(5, 0, 0, 0, 3, 1500, DATE_2024_04_01))
-    current_bot_perf = create_bot_perf("Bot 1", 1600, 120)
+    current_bot_perf = create_bot_perf("Bot 1", 1600, 120, 60)
     update = FullUpdate(previous_row, current_bot_perf, DATE_2025_04_01)
-    self.assertEqual(update.get_rating(), 1600)
     self.assertEqual(update.get_name(), "Bot 1")
+    self.assertEqual(update.get_rating(), 1600)
+    self.assertEqual(update.get_rd(), 60)
     self.assertTrue(update.is_eligible())
     expected_row = LeaderboardRow("Bot 1", current_bot_perf.perf, RankInfo(1, 4, 100, 20, 1, 1600, DATE_2025_04_01))
     self.assertEqual(update.to_leaderboard_row(1), expected_row)
 
   def test_full_update_no_games_played(self) -> None:
-    previous_bot_perf = create_bot_perf("Bot 1", 1500, 100)
+    previous_bot_perf = create_bot_perf("Bot 1", 1500, 100, 45)
     previous_row = LeaderboardRow("Bot 1", previous_bot_perf.perf, RankInfo(5, 0, 0, 0, 3, 1500, DATE_2024_04_01))
     # Reuse the same perf as it has not changed (most crucially, delta games is zero)
     update = FullUpdate(previous_row, previous_bot_perf, DATE_2025_04_01)
